05cc41e300ea6a7e65be0e8e1a39781ff3a2bc3a
picugeo27 <61882288+picugeo27@users.noreply.github.com>
2024-11-14 15:40:50 +0100

    Movimiento tipo grid
    ya se puede mover el jugador y tiene el tipo grid, he actualizado varios documentos, aunque todavia quedan hacer algunas cositas

--- src/scenes/gamescreen.js
+++ src/scenes/gamescreen.js
@@ -1,18 +1,18 @@
 import { Player } from "../objects/player.js";
+import { Coordinates } from "../types/typedef.js";
 
 export class GameScreen extends Phaser.Scene{
     constructor(){
         super({key: 'GameScreen'});
     }
 
-    preload(){
-
-    }
+    /**@type {Coordinates} */
+    #playerCoordinates = new Coordinates(5,5);
 
     create(){
         this.add.image(0,0, 'game_background').setOrigin(0,0).setScale(0.6);
         
-        const player = new Player(this, this.scale.width/2, this.scale.height/2);
+        const player = new Player(this, this.#playerCoordinates);
         
         //this.#cursorKeys = this.input.keyboard.createCursorKeys();
         //console.log(this.#cursorKeys)
